57b97ac807ef80c38ee0b30cda9204fdcad7ffd7
malindu-at-832266882009 <malindupr@cf.lk>
2022-11-24 14:56:18 +0530

    Routing implemented

---
+++ src/routes/RouteLayout.js
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const RouteLayout = () => {
+  return (
+    <div>
+      Outlet
+      <Outlet />
+    </div>
+  );
+};
+export default RouteLayout;
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -4,16 +4,19 @@ import DashboaardScreen from '../pages/dashboard_screen';
 import LandingScreen from '../pages/landing_screen';
 import SignInScreen from '../pages/signin_screen';
 import SignUpScreen from '../pages/signup_screen';
+import RouteLayout from './RouteLayout';
 
 export class PageRoutes extends Component {
   render() {
     return (
       <Router>
         <Routes>
-          <Route path='/' element={<LandingScreen />} />
-          <Route path='/signin' element={<SignInScreen />} />
-          <Route path='/signup' element={<SignUpScreen />} />
-          <Route path='/dashboard' element={<DashboaardScreen />} />
+          <Route path='/' element={<RouteLayout />}>
+            <Route index element={<LandingScreen />} />
+            <Route path='/signin' element={<SignInScreen />} />
+            <Route path='/signup' element={<SignUpScreen />} />
+            <Route path='/dashboard' element={<DashboaardScreen />} />
+          </Route>
         </Routes>
       </Router>
     )
